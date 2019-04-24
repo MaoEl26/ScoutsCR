a1ee88b50f92dbdab0b642fc4631403c45150be2
MiguelJack <miguelfenix16@gmail.com>
2019-04-24 15:35:33 -0600

    mantenimiento alergia
    agregar y editar
    pendiente ver si borramos alergias o no

---
+++ pages/js/mantenimientoAlergia.js
@@ -0,0 +1,83 @@
+$( document ).ready(function() {
+    cargarAlergias();
+});
+
+var arrayAlergia = [];
+
+function cargarAlergias(){
+	var parametros = {
+		opcion : "cargarAlergias"
+	}
+
+	var post = $.post(
+                         "php/mysql.php",    // Script que se ejecuta en el servidor
+	                     parametros,    	                       
+	                     siRespuestacargarAlergias    // Función que se ejecuta cuando el servidor responde
+                         );
+}
+
+function siRespuestacargarAlergias(r){
+	var doc = JSON.parse(r);
+	var salida = '<select class="form-control" tabindex="-1" id="cbAlergia">';                   
+	$("#cbAlergia").html("");
+	for (var i = 0; i < doc.length; i++) {
+        var j = i;
+        var obj = doc[i];
+        salida += '<option value="'+i+'">'+obj.Descripcion+'</option>';
+        arrayAlergia[i] = obj.idAlergia;
+    }
+    salida += "</select>";
+    $("#cbAlergia").html(salida);
+}
+
+function agregarAlergia(){
+	if (document.getElementById('txtNombre').value == "") {
+		alert('El nombre de la alergia es requerido');
+	}else{	
+		var parametros = {
+	        opcion : "agregarAlergia",
+	        txtNombre: $('#txtNombre').val(),
+	    };
+
+	    // Realizar la petición
+		var post = $.post(
+	                          "php/mysql.php",    // Script que se ejecuta en el servidor
+		                      parametros,    	                       
+		                      siRespuestaagregarAlergia    // Función que se ejecuta cuando el servidor responde
+	                          );
+	}
+}
+
+function siRespuestaagregarAlergia(r){
+	limpiar();
+	cargarAlergias();
+	alert(r);
+}
+
+function limpiar(){
+	document.getElementById('txtNombre').value = "";
+	document.getElementById('txtNombreMod').value = "";
+}
+
+function editarAlergia(){
+    var id = arrayAlergia[document.getElementById('cbAlergia').selectedIndex];
+    console.log(id);
+    var parametros = {
+        opcion : "editarAlergia",
+        id : id,
+        txtNombre: $('#txtNombreMod').val(),
+    };
+
+    // Realizar la petición
+    var post = $.post(
+                          "php/mysql.php",    // Script que se ejecuta en el servidor
+                          parametros,                              
+                          siRespuestaeditarAlergia    // Función que se ejecuta cuando el servidor responde
+                          );  
+}
+
+function siRespuestaeditarAlergia(r){
+    limpiar();
+    alert(r);
+    cargarAlergias();
+}
--- pages/php/mysql.php
+++ pages/php/mysql.php
@@ -22,7 +22,7 @@ switch ($opcion) {
 		if(!$mysqli->query("CALL agregarEnfermedad (@nombre)"))
 		{
     		if($mysqli) $mysqli->close(); // Close DB connection
-    		header('HTTP/1.1 400 Es posible que la enfermedad');
+    		header('HTTP/1.1 400 Es posible que la enfermedad ya exista');
     		die();
 		}
 		if($mysqli) $mysqli->close();
@@ -51,6 +51,43 @@ switch ($opcion) {
 		}
 		if($mysqli) $mysqli->close();
 		echo "Enfermedad actualizada";
+	break;
+
+	case 'agregarAlergia':
+		$nombre = $_POST['txtNombre'];
+		$mysqli->query("SET @nombre  = " . "'" . $mysqli->real_escape_string($nombre) . "'");
+		if(!$mysqli->query("CALL agregarAlergia (@nombre)"))
+		{
+    		if($mysqli) $mysqli->close(); // Close DB connection
+    		header('HTTP/1.1 400 Es posible que la alergia ya exista');
+    		die();
+		}
+		if($mysqli) $mysqli->close();
+		echo "Alergia agregada";
+	break;
+
+	case  'cargarAlergias':
+		$resultado = $mysqli->query("CALL cargarAlergias()");
+		$json = array();
+		while($row = $resultado->fetch_array()){
+			$json[] = $row;
+		}
+		echo json_encode($json) ;
+	break;
+
+	case 'editarAlergia':
+		$id = $_POST['id'];
+		$nombre = $_POST['txtNombre'];
+		$mysqli->query("SET @id  = " . "'" . $mysqli->real_escape_string($id) . "'");
+		$mysqli->query("SET @nombre  = " . "'" . $mysqli->real_escape_string($nombre) . "'");
+		if(!$mysqli->query("CALL editarAlergia (@id,@nombre)"))
+		{
+    		if($mysqli) $mysqli->close(); // Close DB connection
+    		header('HTTP/1.1 400 Es posible que la alergia ya exista');
+    		die();
+		}
+		if($mysqli) $mysqli->close();
+		echo "Alergia actualizada";
 	break;
 
 	default:
